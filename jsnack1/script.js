b1230f7a05ec4afed0a684c9ea92b9d108bc7676
samuelebruni <samuele_bruni@hotmail.com>
2023-08-30 10:53:21 +0200

    while

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,6 +1,8 @@
 // Snack 1: L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore.
 
-const numberOne = Number(prompt('Inserisci il primo numero'))
+
+
+/* const numberOne = Number(prompt('Inserisci il primo numero'))
 const numberTwo = Number(prompt('Inserisci il secondo numero'))
 
 console.log('Il primo numero è ', numberOne, 'mentre il secondo numero è ', numberTwo);
@@ -14,6 +16,31 @@ if (numberOne < numberTwo){
 } else{
     document.querySelector('h1').innerHTML = `Il primo numero che hai selezionato`+ ' ' + numberTwo + ' ' + 'ha lo stesso valore del secondo numero che hai selezionato' + ' ' + numberOne;
     console.log(numberOne, 'ha lo stesso valore di', numberTwo);
+} */
+
+//Da fare con WHILE tutti I prossimi snacks. Snack 1. L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore.
+
+let twoAnswer = 0
+while (twoAnswer < 1){
+    const numberOne = Number(prompt('Inserisci il primo numero'))
+    const numberTwo = Number(prompt('Inserisci il secondo numero'))
+    console.log('Il primo numero è ', numberOne, 'mentre il secondo numero è ', numberTwo);
+
+    twoAnswer++
+
+if (numberOne < numberTwo){
+    document.querySelector('h1').innerHTML = `IL NUMERO MAGGIORE SELEZIONATO è`+ ' ' +  numberTwo;
+    console.log(numberTwo, 'è maggiore di', numberOne);
+} else if (numberOne > numberTwo){
+    document.querySelector('h1').innerHTML = `IL NUMERO MAGGIORE SELEZIONATO è`+ ' ' +  numberOne;
+    console.log(numberOne, 'è maggiore di', numberTwo);
+} else{
+    document.querySelector('h1').innerHTML = `Il primo numero che hai selezionato`+ ' ' + numberTwo + ' ' + 'ha lo stesso valore del secondo numero che hai selezionato' + ' ' + numberOne;
+    console.log(numberOne, 'ha lo stesso valore di', numberTwo);
 }
+}
+
+
+
 
 //finish
